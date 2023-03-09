c9fe5b17d6c65d595f8682875bcc0844af18f445
Cameron Ricks <cameron6x6@gmail.com>
2023-03-08 20:34:45 -0700

    starter-demo

---
+++ starter.js
@@ -0,0 +1,151 @@
+////////////////////////
+////// CALCULATOR //////
+////////////////////////
+
+// CODE HERe
+
+const add = (num1, num2) => num1 + num2
+
+const subtract = (num1, num2) => {
+  return num1 - num2
+}
+
+const multiply = function(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2){
+  return num1/ num2
+}
+
+// console.log(add(6, 3))
+
+const calculator = (num1, num2, callback) => {
+  if(Number(num1) && +num2){
+  num1 = +num1
+  num2 = +num2
+
+return callback(num1, num2)
+}else{
+  console.log(`You need to send in numbers only.`)
+}
+}
+
+const result = calculator(6, 3, add)
+// console.log(result)
+const result2 = calculator(`9`, 7, multiply)
+// console.log(result2)
+const result3 = calculator(6, 3, (num1, num2) => num1 + num2)
+// console.log(result3)
+
+///////////////////////
+////// PET STORE //////
+///////////////////////
+
+const dogProducts = [
+    {
+      name: 'leash',
+      colors: ['red', 'blue', 'green'],
+      category: 1,
+      inventory: 30,
+      basePrice: 13.99, 
+      displayPrice: 13.99
+    }, 
+    {
+      name: 'chew toy',
+      colors: ['brown'],
+      category: 2,
+      inventory: 120,
+      basePrice: 6.00, 
+      displayPrice: 6.00
+    }, 
+    {
+      name: 'rope',
+      colors: ['blue & green', 'red & yellow'],
+      category: 2,
+      inventory: 75,
+      basePrice: 4.99, 
+      displayPrice: 4.99
+    }
+]
+
+const catProducts = [
+  {
+    name: 'mouse toy', 
+    colors: ['pink', 'grey', 'black'], 
+    category: 2, 
+    inventory: 125, 
+    basePrice: 2.50, 
+    displayPrice: 2.50
+  },
+  {
+    name: 'cat sweater',
+    colors: ['black'],
+    category: 1,
+    inventory: 15,
+    basePrice: 10.00, 
+    displayPrice: 10.00
+  }, 
+  {
+    name: 'straching post',
+    colors: ['tan'],
+    category: 2,
+    inventory: 40,
+    basePrice: 22.99, 
+    displayPrice: 22.99
+  }
+]
+
+// CODE HERE
+
+const applyPercentDiscount = (product, discount) => {
+product.displayPrice = product.basePrice * (1 - discount)
+}
+// applyPercentDiscount(catProducts[1], .15)
+// console.log(catProducts)
+const applyFlatRateDiscount = (product, discount) => {
+  product.displayPrice  = product.basePrice - discount
+}
+// applyFlatRateDiscount(dogProducts[0], 3)
+// console.log(dogProducts[0])
+
+function applyDiscount(arr, callback, discount){
+  for(let i = 0; i < arr.length; i++){
+    callback(arr[i], discount)
+  }
+}
+// applyDiscount(dogProducts, applyFlatRateDiscount, 1)
+// console.log(dogProducts)
+
+function applyDiscountsByInv(arr, callback, amount, discount){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].inventory < amount){
+      callback(arr[i], discount)
+    }
+  }
+}
+// applyDiscountsByInv(catProducts, applyPercentDiscount, 20, .50)
+// console.log(catProducts)
+
+
+////////////////////////
+////// SANDWICHES //////
+////////////////////////
+
+// CODE HERE
+
+function makeSandwhich(bread){
+  return(ingredients) =>{
+    let order = `you order a ${bread} bread sandwhich with `
+    for(let i = 0; i < ingredients.length; i++){
+      order += `${ingredients[i] } `
+    }
+    return order 
+  }
+}
+
+const makeWhiteSandwhich = makeSandwhich(`white`)
+console.log(makeWhiteSandwhich([`bacon`, `lettuce`]))
+
+const makeIdiotSandwhich = makeSandwhich("Chef Ramsey's hands")
+console.log(makeIdiotSandwhich([`idiot chef`, `chef ramsey`]))
